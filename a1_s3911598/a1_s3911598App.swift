5b6a1afa81a0ce4289bb6bcd3dbe4f0caa1f8a25
Lea Wang <s3911598@student.rmit.edu.au>
2024-09-01 17:52:11 +1000

    Initial commit

---
+++ a1_s3911598/a1_s3911598App.swift
@@ -0,0 +1,17 @@
+//
+//  a1_s3911598App.swift
+//  a1_s3911598
+//
+//  Created by Lea Wang on 27/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct a1_s3911598App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
